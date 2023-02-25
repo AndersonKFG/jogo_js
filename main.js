910fe995b808192a91d76cb3471c49fae6c74a01
Anderson Kauan <anderson.kfgoveia@gmail.com>
2023-02-25 19:24:47 -0300

    Versão 3 - Mineradora Nv1

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const upgrade_gpk = document.getElementById('upgrade_gpk');
 const capacidade_max = document.getElementById('capacidade_max')
 const limite_alerta = document.getElementById('limite_alerta')
 const aumentar_capacidade = document.getElementById('aumentar_capacidade')
+const minerador_1 = document.getElementById('minerador_1')
+const minerador1_ql = document.getElementById('minerador1_ql')
+
 var money = 0;
 
 var ganho_gpk = 1
@@ -11,6 +14,41 @@ var valor_upgrad_gpk = 2
 
 var money_max = 10
 var valor_upgrad_max = 10
+
+//mineradora nv.1
+var valor_m1 = 15
+var qnt_m1 = 0
+var m1_mineram = 0
+
+function m1_automine() {
+    setTimeout(function () {
+        if(money < money_max){
+            money = money + qnt_m1 * 0.1
+        }
+
+        dinheiro_atual.textContent = 'DINHEIRO ATUAL → $$' + money.toFixed(2);
+        alertar()
+        m1_automine()
+    }, 1000);
+}
+
+minerador_1.addEventListener('click',function(){
+    if(money >= valor_m1){
+        console.log("OLÁ")
+        money = money - valor_m1
+        dinheiro_atual.textContent = 'DINHEIRO ATUAL → $$' + money.toFixed(2);
+        qnt_m1 = qnt_m1 + 1
+        valor_m1 = valor_m1 + valor_m1 * 25 / 100
+        minerador_1.innerHTML = 'COMPRAR 1 MINERADOR - CUSTO: $$' + valor_m1.toFixed(2) +' <br> Uma mineradora básica, barata o suficiente para ser suspeita.'
+        minerador1_ql.textContent = 'QNT MINERADORES NV.1 → '+qnt_m1+' | MINERAM P/S → $$'+(qnt_m1 * 0.1).toFixed(1)
+
+        alertar()
+    }
+
+    if(qnt_m1 == 1){
+        m1_automine()
+    }
+})
 
 gpk.addEventListener('click',function(){
     money = money + ganho_gpk;
